26c4e4c4ae9a2f6e41bb325473e9d028684d1175
isak.savo <isak.savo@e736173e-0368-11de-9d1d-173cc1d06e3c>
2009-03-09 20:07:45 +0000

    Add beginning of banshee importer class.

--- trunk/WinampMigrator/Migrator.cs
+++ trunk/WinampMigrator/Migrator.cs
@@ -8,40 +8,74 @@ namespace WinampMigrator
 	public class Migrator
 	{
 		public static string ProgramName { get { return "winamp-migrator"; } }
-		private bool dryRun = true;
-		private int verbosity;
+		private bool dryRun = true;		
 		private string bansheeDb;
 		private WinampDatabase databaseFiles;
 		
 		public Migrator(string[] args)
 		{
-			ParseCommandLine(args);			
-			Table tbl = null;
+			ParseCommandLine(args);
+			if (bansheeDb == null)
+				bansheeDb = FindBansheeDb();
+			if (bansheeDb == null)
+			{
+				Console.Error.WriteLine("Couldn't find Banshee db. Use --banshee-db to specify it");
+				Environment.Exit((int)ExitCodes.InvalidBansheeDatabase);
+			}
+			// Try to open here to make sure the db exists and is valid
 			try
 			{
-				tbl = new Table(databaseFiles);
-			} catch (ArgumentException ex)
+				var tbl = new Table(databaseFiles);
+				tbl.Close();
+			} 
+			catch (ArgumentException ex)
 			{
 				Console.Error.WriteLine("Invalid winamp database specified: " + ex.Message);
 				Environment.Exit((int)ExitCodes.InvalidWinampDatabase);
 			}			
-			LogMessage(1, "Number of files in winamp db: {0}", tbl.NumFiles);
+		}
+		
+		public void Migrate()
+		{
+			using (BansheeDatabase banshee = new BansheeDatabase(bansheeDb))
+			using (Table tbl = new Table(databaseFiles))
+			{
+				Logger.LogMessage(1, "Winamp DB successfully opened");
+				foreach (Record row in tbl.Records)
+				{
+					StringField title  = row.GetFieldByType(MetadataField.Title) as StringField;
+					StringField artist = row.GetFieldByType(MetadataField.Artist) as StringField;
+					StringField album  = row.GetFieldByType(MetadataField.Album) as StringField;
+					if (title == null || artist == null || album == null)
+					{
+						Logger.LogMessage(0, "Record does not contain title, artist & album");
+						continue;
+					}
+					var track = banshee.GetTrack(title.Value, artist.Value, album.Value);
+					if (track == null)
+					{
+						Logger.LogMessage(0, "Failed to find banshee track for {0} - {1} ({2})", artist, title, album);
+						continue;
+					}
+					Logger.LogMessage(2, "Found Banshee Track ({0}) for {1} - {2} ({2})", track, artist, title, album);
+				}
+			}
+		}
+				
+		private string FindBansheeDb()
+		{
+			string confDir = Environment.GetEnvironmentVariable("XDG_CONFIG_HOME");
+			if (String.IsNullOrEmpty(confDir))
+				confDir = System.IO.Path.Combine(Environment.GetEnvironmentVariable("HOME"), ".config");			
 			
-			tbl.Close();
+			foreach (string dir in System.IO.Directory.GetDirectories(confDir))
+			{
+				string dirName = System.IO.Path.GetFileName(dir);
+				if (dirName.StartsWith("banshee", StringComparison.InvariantCultureIgnoreCase))
+					return System.IO.Path.Combine(dir, "banshee.db");
+			}
+			return null;
 		}
-		
-		private void LogMessage(int atLevel, string format, params object[] args)
-		{
-			LogMessage(atLevel, String.Format(format, args));
-		}
-		
-		private void LogMessage(int atLevel, string message)
-		{
-			if (verbosity < atLevel)
-				return;
-			Console.WriteLine(message);
-		}
-		
 		/// <summary>
 		/// Parses the command line options. This method will exit the program if it encounters any errors or invalid args.
 		/// </summary>
@@ -56,7 +90,7 @@ namespace WinampMigrator
 				{ "dry-run", "Don't write to Banshee DB, only simulate", v => dryRun = true },
 				{ "h|help", "Show this help and then exit", v => show_help = (v != null) },
 				{ "banshee-db=", "Specify the banshee db (default is $XDG_CONFIG_HOME/banshee-1/banshee.db)", v => bansheeDb = v },
-				{ "V|verbose", "Increase verbosity (specify multiple times to increase further)", v => { if (v != null) verbosity++; } },
+				{ "V|verbose", "Increase verbosity (specify multiple times to increase further)", v => { if (v != null) Logger.Verbosity++; } },
 			};
 			
 			List<string> extra = null;
@@ -106,20 +140,23 @@ namespace WinampMigrator
 		{
 			Console.WriteLine("Usage: {0} [OPTIONS] <WinampDb> [<WinampDbIndex>]", ProgramName);
 			Console.WriteLine(" Where OPTIONS is one or more of the following:");
-			opts.WriteOptionDescriptions(Console.Out);				
+			opts.WriteOptionDescriptions(Console.Out);
+			Console.WriteLine();
+			Console.WriteLine("If no index is specified, it is assumed to reside in the same directory as the Winamp DB and have the extension .idx");
 		}
 		
 		static void Main(string[] args)
 		{
 			try
 			{	
-				new Migrator(args);
+				var migrator = new Migrator(args);
+				migrator.Migrate();
 			}
-			catch (Exception ex)
+			catch (ApplicationException ex)
 			{
 				Console.Error.WriteLine("Error migrating data: " + ex.Message);
 				Environment.Exit((int) ExitCodes.Failed);
-			}
+			}			
 		}
 		
 		private enum ExitCodes : int
@@ -128,6 +165,25 @@ namespace WinampMigrator
 			Failed,
 			InvalidArguments,
 			InvalidWinampDatabase,
+			InvalidBansheeDatabase,
 		}
+	}
+	
+	public sealed class Logger
+	{
+		public static int Verbosity { get; set; }
+
+		public static void LogMessage(int atLevel, string format, params object[] args)
+		{
+			LogMessage(atLevel, String.Format(format, args));
+		}
+		
+		public static void LogMessage(int atLevel, string message)
+		{
+			if (Verbosity < atLevel)
+				return;
+			Console.WriteLine(message);
+		}
+		
 	}
 }
